690b79ee99aa898015c8a026ae6c1dca1f3c2a1f
SitiZahraSifa11 <160223744+SitiZahraSifa11@users.noreply.github.com>
2024-05-20 10:51:20 +0700

    Update SoalNo1.py

--- SoalNo1.py
+++ SoalNo1.py
@@ -1,16 +1,17 @@
+import array as arr
 # No.1
 # Ilham memiliki sebuah list yang berisi angka-angka acak. Anda ingin menghapus 
 # semua angka yang memiliki nilai kurang dari 5 dan menggantinya dengan nilai 0, 
 # dan akan mengurutkan dari nilai yang terbesar ke yang terkecil. Bantu Ilham untuk 
 # menyelesaikan persoalan tersebut menggunakan array method.
 # Input: [8, 3, 12, 4, 7, 2]
-# Output: [12, 8, 7, 4, 0, 0]
+# Output: [12, 8, 7, 0, 0, 0]
 
 
-arr = [8,3,12,4,7,2]
+arr = ('i',[8,3,12,4,7,2])
 
 hasil=[]
-for n in arr:
+for n in arr[1]:
     if n < 5:
         hasil.append(0)
     else:
@@ -18,8 +19,6 @@ for n in arr:
 
 hasil.sort(reverse=True)
 print("Output:", hasil)
-
-
 
 
 
