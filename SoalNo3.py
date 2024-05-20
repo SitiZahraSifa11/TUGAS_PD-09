cb0f266a4969a3c373e430df348972a8c4c1708c
SitiZahraSifa11 <160223744+SitiZahraSifa11@users.noreply.github.com>
2024-05-20 12:06:02 +0700

    Update SoalNo3.py

--- SoalNo3.py
+++ SoalNo3.py
@@ -6,11 +6,10 @@ import array as arr
 # Input: kata = ["apel", "jeruk", "mangga", "pisang", "anggur", "durian"]
 # Output: ['anggur', 'durian', 'jeruk', 'mangga', 'pisang']
 
-kata_int = ('u',["apel", "jeruk", "mangga", "pisang", "anggur", "durian"])
+katanya = ["apel", "jeruk", "mangga", "pisang", "anggur", "durian"]
 output=[]
-for kata in kata_int[1]:  
+for kata in katanya:  
     if len(kata) >= 5:
         output.append(kata)
 output.sort()
 print("Output : ",output)
-
